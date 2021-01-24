e53f099cb1e863d70dac8f693a82862af5ebff37
Ivanov Sergey (GeekBrains) <kiwiz697@gmail.com>
2021-01-24 11:41:45 +0300

    Lesson_6
    Scrapy

---
+++ Lesson_6/items.py
@@ -0,0 +1,35 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+from itemloaders.processors import MapCompose, TakeFirst
+
+
+def process_price_main(price_main):
+    try:
+        price_main = int(price_main.replace(' ð.', '').replace(' ', ''))
+        return price_main
+    except:
+        return price_main
+
+
+def process_price_discount(price_discount):
+    try:
+        price_discount = int(price_discount.replace(' ð.', '').replace(' ', ''))
+        return price_discount
+    except:
+        return price_discount
+
+
+class JobparserItem(scrapy.Item):
+    name = scrapy.Field(output_processor=TakeFirst())
+    url = scrapy.Field(output_processor=TakeFirst())
+    auth = scrapy.Field(output_processor=TakeFirst())
+    price_main = scrapy.Field(input_processor=MapCompose(process_price_main), output_processor=TakeFirst())
+    price_discount = scrapy.Field(input_processor=MapCompose(process_price_discount), output_processor=TakeFirst())
+    rate = scrapy.Field(output_processor=TakeFirst())
+    _id = scrapy.Field()
+    print()
---
+++ Lesson_6/spiders/book24_ru.py
@@ -0,0 +1,48 @@
+import scrapy
+from scrapy.http import HtmlResponse
+from jobparser.items import JobparserItem
+from scrapy.loader import ItemLoader
+
+
+class Book24ruSpider(scrapy.Spider):
+    name = 'book24ru'
+    allowed_domains = ['book24.ru']
+    start_urls = ['https://book24.ru/search/?q=business']
+
+    def parse(self, response: HtmlResponse):
+        book_links = response.xpath("//a[@class = 'book-preview__title-link']/@href").extract()
+        for link in book_links:
+            yield response.follow(link, callback=self.book24_parse)
+        next_page = response.xpath("//div[@class ='catalog-pagination__list']/a[contains(text(),'Далее')]/@href").extract_first()
+        if next_page:
+            yield response.follow(next_page, self.parse)
+        print()
+
+
+
+    def book24_parse(self, response: HtmlResponse):
+        loader = ItemLoader(item=JobparserItem(), response=response)
+        loader.add_xpath('name', "//h1/text()")
+        loader.add_value('url', response.url)
+        loader.add_xpath('auth', "//span[@class = 'item-tab__chars-value']/a[@itemprop='author']/text()")
+        loader.add_xpath('price_main', "//div[@class = 'item-actions__price-old']/text()")
+        loader.add_xpath('price_discount', "//b[@itemprop = 'price']/text()")
+        loader.add_xpath('rate', "//span[@class = 'rating__rate-value']/text()")
+
+        yield loader.load_item()
+
+        print()
+
+
+
+
+        #book24_name = response.xpath("//h1/text()").extract_first()
+        # book24_url = response.url
+        # book24_auth = response.xpath("//span[@class = 'item-tab__chars-value']/a[@itemprop='author']/text()").extract()
+        # book24_price_main = response.xpath("//div[@class = 'item-actions__price-old']/text()").extract_first()
+        # book24_price_discount = response.xpath("//b[@itemprop = 'price']/text()").extract_first()
+        # book24_rate = response.xpath("//span[@class = 'rating__rate-value']/text()").extract_first()
+
+        # yield JobparserItem(name=book24_name, url=book24_url, auth=book24_auth, price_main=book24_price_main,
+                            #price_discount=book24_price_discount, price_rate=book24_rate)
+
